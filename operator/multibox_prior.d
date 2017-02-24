21f39c078502f1923d953671df2c105bfc50575d
ellin <zhoury@hitrobotgroup.com>
2017-02-24 11:18:42 +0800

    commit src

---
+++ operator/multibox_prior.d
@@ -0,0 +1,128 @@
+example/ssd/operator/multibox_prior.o: \
+ example/ssd/operator/multibox_prior.cc \
+ example/ssd/operator/./multibox_prior-inl.h \
+ /opt/mxnet/dmlc-core/include/dmlc/logging.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/parameter.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./json.h \
+ /opt/mxnet/dmlc-core/include/dmlc/././base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/././logging.h \
+ /opt/mxnet/dmlc-core/include/dmlc/././type_traits.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./././base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/././any.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./././logging.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./logging.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./type_traits.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./optional.h include/mxnet/operator.h \
+ /opt/mxnet/dmlc-core/include/dmlc/base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/json.h \
+ /opt/mxnet/dmlc-core/include/dmlc/registry.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./parameter.h \
+ /opt/mxnet/nnvm/include/nnvm/node.h \
+ /opt/mxnet/nnvm/include/nnvm/./base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/any.h \
+ /opt/mxnet/dmlc-core/include/dmlc/memory.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./thread_local.h \
+ /opt/mxnet/dmlc-core/include/dmlc/array_view.h \
+ /opt/mxnet/nnvm/include/nnvm/./op.h \
+ /opt/mxnet/nnvm/include/nnvm/././base.h include/mxnet/./base.h \
+ /opt/mxnet/dmlc-core/include/dmlc/io.h \
+ /opt/mxnet/dmlc-core/include/dmlc/./serializer.h \
+ /opt/mxnet/dmlc-core/include/dmlc/././io.h \
+ /opt/mxnet/dmlc-core/include/dmlc/type_traits.h \
+ /opt/mxnet/mshadow/mshadow/tensor.h /opt/mxnet/mshadow/mshadow/./base.h \
+ /usr/include/openblas/cblas.h /usr/include/openblas/openblas_config.h \
+ /usr/local/cuda/include/cuda.h /usr/local/cuda/include/cublas_v2.h \
+ /usr/local/cuda/include/cublas_api.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/host_defines.h \
+ /usr/local/cuda/include/cuComplex.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h \
+ /usr/local/cuda/include/cuda_fp16.h \
+ /usr/local/cuda/include/library_types.h /usr/local/cuda/include/curand.h \
+ /usr/local/cuda/include/cuda_runtime.h \
+ /usr/local/cuda/include/host_config.h \
+ /usr/local/cuda/include/channel_descriptor.h \
+ /usr/local/cuda/include/cuda_runtime_api.h \
+ /usr/local/cuda/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/include/driver_functions.h \
+ /usr/local/cuda/include/vector_functions.h \
+ /usr/local/cuda/include/vector_functions.hpp \
+ /usr/local/cuda/include/cudnn.h /opt/mxnet/mshadow/mshadow/././half.h \
+ /opt/mxnet/mshadow/mshadow/./././base.h \
+ /usr/local/cuda/include/cuda_fp16.h \
+ /opt/mxnet/mshadow/mshadow/././logging.h \
+ /opt/mxnet/mshadow/mshadow/./expression.h \
+ /opt/mxnet/mshadow/mshadow/././base.h \
+ /opt/mxnet/mshadow/mshadow/./stream_gpu-inl.h \
+ /opt/mxnet/mshadow/mshadow/././tensor.h \
+ /opt/mxnet/mshadow/mshadow/./extension.h \
+ /opt/mxnet/mshadow/mshadow/././expr_engine-inl.h \
+ /opt/mxnet/mshadow/mshadow/./././logging.h \
+ /opt/mxnet/mshadow/mshadow/./././expression.h \
+ /opt/mxnet/mshadow/mshadow/./././tensor.h \
+ /opt/mxnet/mshadow/mshadow/./././dot_engine-inl.h \
+ /opt/mxnet/mshadow/mshadow/././././base.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/implicit_gemm.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../extension.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../packet-inl.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/.././base.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/.././tensor.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/.././expression.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../packet/plain-inl.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../packet/../base.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../packet/../packet-inl.h \
+ /opt/mxnet/mshadow/mshadow/././././extension/../packet/sse-inl.h \
+ /opt/mxnet/mshadow/mshadow/././extension/broadcast.h \
+ /opt/mxnet/mshadow/mshadow/././extension/../extension.h \
+ /opt/mxnet/mshadow/mshadow/././extension/unpack_patch2col.h \
+ /opt/mxnet/mshadow/mshadow/././extension/pack_col2patch.h \
+ /opt/mxnet/mshadow/mshadow/././extension/reshape.h \
+ /opt/mxnet/mshadow/mshadow/././extension/swapaxis.h \
+ /opt/mxnet/mshadow/mshadow/././extension/reduceto1d.h \
+ /opt/mxnet/mshadow/mshadow/././extension/spatial_pool.h \
+ /opt/mxnet/mshadow/mshadow/././extension/spatial_unpool.h \
+ /opt/mxnet/mshadow/mshadow/././extension/channel_pool.h \
+ /opt/mxnet/mshadow/mshadow/././extension/channel_unpool.h \
+ /opt/mxnet/mshadow/mshadow/././extension/pad.h \
+ /opt/mxnet/mshadow/mshadow/././extension/crop.h \
+ /opt/mxnet/mshadow/mshadow/././extension/mirror.h \
+ /opt/mxnet/mshadow/mshadow/././extension/concat.h \
+ /opt/mxnet/mshadow/mshadow/././extension/implicit_gemm.h \
+ /opt/mxnet/mshadow/mshadow/././extension/choose.h \
+ /opt/mxnet/mshadow/mshadow/././extension/fill.h \
+ /opt/mxnet/mshadow/mshadow/././extension/one_hot.h \
+ /opt/mxnet/mshadow/mshadow/././extension/slice.h \
+ /opt/mxnet/mshadow/mshadow/././extension/slice_ex.h \
+ /opt/mxnet/mshadow/mshadow/././extension/take.h \
+ /opt/mxnet/mshadow/mshadow/././extension/take_grad.h \
+ /opt/mxnet/mshadow/mshadow/././extension/reduce_with_axis.h \
+ /opt/mxnet/mshadow/mshadow/././extension/broadcast_with_axis.h \
+ /opt/mxnet/mshadow/mshadow/././extension/spatial_upsampling_nearest.h \
+ /opt/mxnet/mshadow/mshadow/././extension/transpose.h \
+ /opt/mxnet/mshadow/mshadow/././extension/flip.h \
+ /opt/mxnet/mshadow/mshadow/././extension/complex.h \
+ /opt/mxnet/mshadow/mshadow/././extension/range.h \
+ /opt/mxnet/mshadow/mshadow/././extension/mask.h \
+ /opt/mxnet/mshadow/mshadow/./expr_engine-inl.h \
+ /opt/mxnet/mshadow/mshadow/./tensor_cpu-inl.h \
+ /opt/mxnet/mshadow/mshadow/././packet-inl.h \
+ /opt/mxnet/mshadow/mshadow/././dot_engine-inl.h \
+ /opt/mxnet/mshadow/mshadow/./tensor_gpu-inl.h \
+ /opt/mxnet/mshadow/mshadow/./io.h \
+ /opt/mxnet/mshadow/mshadow/./tensor_container.h \
+ /opt/mxnet/mshadow/mshadow/././io.h \
+ /opt/mxnet/mshadow/mshadow/./tensor_blob.h \
+ /opt/mxnet/mshadow/mshadow/./random.h \
+ /opt/mxnet/mshadow/mshadow/././tensor_container.h \
+ /opt/mxnet/mshadow/mshadow/./expr_scalar-inl.h \
+ /opt/mxnet/nnvm/include/nnvm/op.h /opt/mxnet/nnvm/include/nnvm/tuple.h \
+ /opt/mxnet/nnvm/include/nnvm/symbolic.h \
+ /opt/mxnet/nnvm/include/nnvm/./node.h include/mxnet/././tensor_blob.h \
+ include/mxnet/./././base.h include/mxnet/./resource.h \
+ include/mxnet/././base.h include/mxnet/././engine.h include/mxnet/base.h \
+ src/operator/./operator_common.h
---
+++ tools/visualize_net_json.py
@@ -0,0 +1,37 @@
+import find_mxnet
+import mxnet as mx
+import importlib
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='network visualization')
+parser.add_argument('--network', type=str, default='vgg16_reduced',
+                    choices = ['vgg16_reduced'],
+                    help = 'the cnn to use')
+parser.add_argument('--num-classes', type=int, default=20,
+                    help='the number of classes')
+parser.add_argument('--data-shape', type=int, default=300,
+                    help='set image\'s shape')
+parser.add_argument('--train', action='store_true', default=False, help='show train net')
+args = parser.parse_args()
+
+sys.path.append('../model')
+
+# train net
+net = mx.symbol.load('ssd_300-symbol.json')
+
+cls_preds = net.get_internals()["multibox_cls_pred_output"]
+loc_preds = net.get_internals()["multibox_loc_pred_output"]
+anchor_boxes = net.get_internals()["multibox_anchors_output"]
+
+cls_prob = mx.symbol.SoftmaxActivation(data=cls_preds, mode='channel', \
+    name='cls_prob')
+# group output
+# out = mx.symbol.Group([loc_preds, cls_preds, anchor_boxes])
+out = mx.symbol.MultiBoxDetection(*[cls_prob, loc_preds, anchor_boxes], \
+    name="detection", nms_threshold=nms_thresh, force_suppress=force_suppress,
+    variances=(0.1, 0.1, 0.2, 0.2))
+
+a = mx.viz.plot_network(net, shape={"data":(1,3,args.data_shape,args.data_shape)}, \
+    node_attrs={"shape":'rect', "fixedsize":'false'})
+a.render("ssd_" + args.network)
