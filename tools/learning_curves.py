235f1dbb5bdd9d6f56dd5485a4bce0d0deb31675
Zhoury <zhoury@hitrobotgroup.com>
2017-03-23 08:50:43 +0800

    add a argument named --save-name to save plot to different png

--- tools/learning_curves.py
+++ tools/learning_curves.py
@@ -9,6 +9,8 @@ import sys
 parser = argparse.ArgumentParser(description='Parses log file and generates train/val curves')
 parser.add_argument('--log-file', type=str,default="log_tr_va",
                     help='the path of log file')
+parser.add_argument('--save-name', type=str,default="learning_curve.png",
+                    help='the name of save png')
 args = parser.parse_args()
 
 # sys.path.append('../test/170217')
@@ -25,7 +27,7 @@ log_tr = [float(x) for x in TR_RE.findall(log)]
 log_va = [float(x) for x in VA_RE.findall(log)]
 idx = np.arange(len(log_tr))
 
-plt.figure(figsize=(8, 6))
+fig = plt.figure(figsize=(8, 6))
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
 plt.plot(idx, log_tr, 'o', linestyle='-', color="r",
@@ -35,8 +37,8 @@ plt.plot(idx, log_va, 'o', linestyle='-', color="b",
          label="Validation accuracy")
 
 plt.legend(loc="best")
-plt.xticks(np.arange(min(idx), max(idx)+1, 5))
-plt.yticks(np.arange(0, 1, 0.2))
-plt.ylim([0,1])
-plt.show()
-
+# plt.xticks(np.arange(min(idx), max(idx)+1, 5))
+# plt.yticks(np.arange(0, 1, 0.2))
+# plt.ylim([0,1])
+# plt.show()
+fig.savefig(args.save_name)
